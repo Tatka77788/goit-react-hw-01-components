c3ec5ed8371d21c98135f65cf94d6ecc0051f6a9
Tetiana <tatka777t@gmail.com>
2020-04-02 13:02:31 +0300

    fix

---
+++ build/precache-manifest.3bbedb6b98e8811c12a59c302c6b00e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c09565ca0fcd9eb1aed33c0934d8c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f35dfa8e6de2dc01f28",
+    "url": "/static/css/main.e9b99fd0.chunk.css"
+  },
+  {
+    "revision": "d3a0248604f290b0178c",
+    "url": "/static/js/2.364e71f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.364e71f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f35dfa8e6de2dc01f28",
+    "url": "/static/js/main.48b152db.chunk.js"
+  },
+  {
+    "revision": "db046dd09a9447cbe724",
+    "url": "/static/js/runtime-main.7043763e.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.00315a6b609ea681de9ed000deb2e188.js"
+  "/precache-manifest.3bbedb6b98e8811c12a59c302c6b00e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
