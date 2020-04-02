1aabed5677506b2850e02b54898402599aa380dc
Tetiana <tatka777t@gmail.com>
2020-04-02 13:07:18 +0300

    Updates

---
+++ precache-manifest.64ee900fbc215d3ace598c178c04e25f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49ab39c23039d20390ebe7a6ed5cf12",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0f35dfa8e6de2dc01f28",
+    "url": "/goit-react-hw-01-components/static/css/main.e9b99fd0.chunk.css"
+  },
+  {
+    "revision": "d3a0248604f290b0178c",
+    "url": "/goit-react-hw-01-components/static/js/2.364e71f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.364e71f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f35dfa8e6de2dc01f28",
+    "url": "/goit-react-hw-01-components/static/js/main.48b152db.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
